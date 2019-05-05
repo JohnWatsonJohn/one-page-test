904fc1ad04f92726d3d66b00772813ae11d986fc
pasha <gpb@yandex.ru>
2019-05-05 11:01:57 +0400

    init

--- src/app/pages/content/contact-section/contact-section.component.ts
+++ src/app/pages/content/contact-section/contact-section.component.ts
@@ -33,7 +33,7 @@ export class ContactSectionComponent implements OnInit {
     if (this.contItemIcon === 'fa fa-envelope') {
        // this.contItemText= this.a;
         // this.contItemText.innerHTML = ;
-        this.document.getElementById('emailCont').innerHTML = '<a href="mailto:support@bestlooker.pro">support@bestlooker.pro</a>';
+        document.getElementById('emailCont').innerHTML = '<a href="mailto:support@bestlooker.pro">support@bestlooker.pro</a>';
         // console.log(this.a);
     }
   }
